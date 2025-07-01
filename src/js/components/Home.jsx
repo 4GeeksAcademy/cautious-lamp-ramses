c4743e0c9c389df0dc22f59975fcff7358fefe78
ramsesfg <zad.yadiel12@gmail.com>
2025-07-01 02:57:00 +0000

    falta

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+
+
 const Home = () => {
+	let [nuevaTarea, setNuevaTarea] = useState()
+	let [listaDeTareas,setlistaDeTareas] = useState([
+		"practicar react",
+		"bañar al perro",
+		"sacar la basura",
+		"hacer la cena",
+		"hacer las compras",
+
+	])
+	let agregarTarea = (tecla) =>{
+		if (tecla === "Enter"){
+			setlistaDeTareas([...listaDeTareas, nuevaTarea.trim()]);
+		}
+	}
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div>
+				<h1>Todo </h1>
+				<input onChange={event => setNuevaTarea(event.target.value)} type="text" value={nuevaTarea || " "} 
+				onKeyUp={event=>agregarTarea(event.key)}
+				/>
+			</div>
+			{
+				listaDeTareas.map((tarea, index) => {
+					return <p key={index}>{tarea} </p>
+				})
+			}
 		</div>
 	);
 };
